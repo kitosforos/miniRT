4827c00694def0fc4228ee79fd137484da76c564
kitosforos <kitosforos@gmail.com>
2023-06-05 13:47:35 +0200

    first structs

---
+++ inc/main.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maralons <maralons@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 13:44:43 by maralons          #+#    #+#             */
+/*   Updated: 2023/06/05 13:46:31 by maralons         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+typedef struct s_coord
+{
+	double	x;
+	double	y;
+	double	z;
+}				t_coord;
+
+typedef struct s_color
+{
+	int		r;
+	int		g;
+	int		b;
+}				t_color;
+
+#endif
