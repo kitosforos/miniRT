8d71ada6ec5003ccbe3d82e77d25b272940c2394
kitosforos <kitosforos@gmail.com>
2023-06-05 13:54:55 +0200

    main.h structs

--- inc/main.h
+++ inc/main.h
@@ -6,7 +6,7 @@
 /*   By: maralons <maralons@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 13:44:43 by maralons          #+#    #+#             */
-/*   Updated: 2023/06/05 13:46:31 by maralons         ###   ########.fr       */
+/*   Updated: 2023/06/05 13:52:20 by maralons         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,48 @@ typedef struct s_color
 	int		g;
 	int		b;
 }				t_color;
+
+typedef struct s_amblight
+{
+	double	ratio;
+	t_color	color;
+}				t_amblight;
+
+typedef struct s_light
+{
+	t_coord	coord;
+	double	ratio;
+	t_color	color;
+}				t_light;
+
+typedef struct s_camera
+{
+	t_coord	coord;
+	t_coord	ori;
+	double	fov;
+}				t_camera;
+
+typedef struct s_sphere
+{
+	t_coord	coord;
+	double	diametre;
+	t_color	color;
+}				t_sphere;
+
+typedef struct s_plane
+{
+	t_coord	coord;
+	t_coord	dir;
+	t_color	color;
+}				t_plane;
+
+typedef struct s_cylinder
+{
+	t_coord	coord;
+	t_coord	ori;
+	double	diametre;
+	double	height;
+	t_color	color;
+}				t_cylinder;
 
 #endif
